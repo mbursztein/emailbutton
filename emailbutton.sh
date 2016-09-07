1be2fafed4e5e28909922a9778f4b3531a997ed7
Marcelo Bursztein <mbursztein@gmail.com>
2016-09-07 18:59:07 -0400

    Shell script

---
+++ emailbutton.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+ARDUINO_PORT=/dev/ttyACM0
+ARDUINO_SPEED=9600
+
+# Set speed for usb
+stty -F $ARDUINO_PORT ispeed $ARDUINO_SPEED ospeed $ARDUINO_SPEED raw
+exec 6<$ARDUINO_PORT
+
+# Read data from Arduino
+while read -u 6 f ;do
+
+     # Remove trailing carriage return character added
+     # by println to satisfy stupid MS-DOS Computers
+     f=${f:0:${#f} - 1}
+     if [ $f == "1" ]; then
+      echo "Button Pressed" | sendmail mbursztein@gmail.com
+      echo "Button pressed, sending email."
+     fi
+done
